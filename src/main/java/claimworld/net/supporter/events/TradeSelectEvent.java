240a50f8e5ef26d182288b3e0fc2f62c519a74e5
Maciej Panocha <maciej.panocha@gmail.com>
2023-01-26 08:41:24 +0100

    clearing console

--- src/main/java/claimworld/net/supporter/events/TradeSelectEvent.java
+++ src/main/java/claimworld/net/supporter/events/TradeSelectEvent.java
@@ -14,16 +14,12 @@ import static org.bukkit.Bukkit.getScheduler;
 
 public class TradeSelectEvent implements Listener {
 
-    TaskManager taskManager = TaskManager.getInstance();
-
     @EventHandler
     public void tradeSelectEvent(org.bukkit.event.inventory.TradeSelectEvent event) {
         getScheduler().runTaskAsynchronously(Supporter.getPlugin(), () -> {
             Player player = (Player) event.getWhoClicked();
             Location location = player.getLocation();
             getLogger().log(Level.INFO, "Trade press -> " + player.getDisplayName() + " (" + Math.round(location.getX()) + "x, " + Math.round(location.getY()) + "y, " + Math.round(location.getZ()) + "z)");
-
-            taskManager.tryFinishTask(player, taskManager.getTaskMap().get("pressTrader"));
         });
     }
 }
